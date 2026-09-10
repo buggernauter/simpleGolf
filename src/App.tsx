6a5cdcb6bb4e3ebd671d23c69e1985c68dcfcc88
buggernauter <khazar.mehraban@gmail.com>
2026-09-10 16:49:14 +0200

    whip

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import { ThemeProvider } from "styled-components";
 
-import { DesktopNotice } from "./components/desktop-notice/desktop-notice";
+// import { DesktopNotice } from "./components/desktop-notice/desktop-notice";
 import { useThemeMode } from "./hooks/useThemeMode";
 import { StyledGlobalStyle } from "./global-style";
 import { PracticeGreenPage } from "./pages/practice-green-page";
-import { RoundTrackerPage } from "./pages/round-tracker-page";
+// import { RoundTrackerPage } from "./pages/round-tracker-page";
 import { StyledAppLayout } from "./styles/layout";
 
 export default function App() {
   const { theme } = useThemeMode();
-  const isPracticeGreenPage = window.location.pathname === "/ovningsgreen";
+  // const isPracticeGreenPage = window.location.pathname === "/ovningsgreen";
 
   return (
     <ThemeProvider theme={theme}>
       <StyledGlobalStyle />
       <StyledAppLayout>
-        {isPracticeGreenPage ? (
-          <PracticeGreenPage />
-        ) 
-        : (
-          <DesktopNotice>
-            <RoundTrackerPage />
-          </DesktopNotice>
-        )}
+        {/* {isPracticeGreenPage ? ( */}
+        <PracticeGreenPage />
+        {/* )  */}
+        {/* : ( */}
+        {/* <DesktopNotice> */}
+        {/* <RoundTrackerPage /> */}
+        {/* </DesktopNotice> */}
+        {/* )} */}
       </StyledAppLayout>
     </ThemeProvider>
   );
